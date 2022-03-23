a2fbb9c430cf3c209ee90fe6479a843de76171bb
Matteo Villosio <s258803@studenti.polito.it>
2022-03-23 16:24:49 +0100

    Versione 1.0

--- src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,25 +1,128 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
+
+import it.polito.tdp.spellchecker.model.Dictionary;
+import it.polito.tdp.spellchecker.model.RichWord;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class FXMLController {
+	
+	private Dictionary model;
+	private List<String> inputTextList;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML
+    private ComboBox<String> txtCombo;
+
+    @FXML
+    private Label txtError;
+
+    @FXML
+    private TextArea txtInput;
+
+    @FXML
+    private TextArea txtOutput;
+
+    @FXML
+    private Label txtSeconds;
+
+
+    @FXML
+    void doClearText(ActionEvent event) {
+    	
+    	txtOutput.clear();
+    	txtInput.clear();
+    	txtError.setText("Number of errors:");
+    	txtSeconds.setText("Spell check status:");
 
     }
-}
 
+    @FXML
+    void doSpellCheck(ActionEvent event) {
+    	
+    	txtOutput.clear();
+    	inputTextList = new LinkedList<String>();
+    	
+    	if (txtCombo.getValue()==null) {
+    		txtOutput.setText("Seleziona una lingua !!");
+    		return;
+    	}
+    	
+    	if (!model.loadDictionary(txtCombo.getValue())) {
+    		txtInput.setText("Errore nel caricamento del dizionario");
+    		return;
+    	}
+    	
+    	String inputText = txtInput.getText().toLowerCase();
+    	
+    	if(inputText.isEmpty()) {
+    	txtInput.setText("Inserire un testo da correggere!");
+    	return;	
+    	}
+    	
+    	inputText = inputText.replaceAll("\n", " ");
+    	//inputText = inputText.replaceAll(“[.,\\/#!$%\\^&\\*;:{}=\\-_`~()\\[\\]]", "");
+    	
+    	StringTokenizer st = new StringTokenizer(inputText, " ");
+    	
+    	while(st.hasMoreTokens()) {
+    		inputTextList.add(st.nextToken());
+    	}
+    	
+    	List <RichWord> outputTextList = model.spellCheckText(inputTextList);
+    	
+    	int numErrori =0;
+    	StringBuilder richText = new StringBuilder();
+    	
+    	for (RichWord r: outputTextList) {
+    		if(!r.isCorrect()) {
+    			numErrori++;
+    			richText.append(r.getWord()+"\n");
+    		}
+    	}
+    	
+    	txtOutput.setText(richText.toString());
+    	txtError.setText("The text contains" +numErrori+" errors");
+    		
+    	
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtCombo != null : "fx:id=\"txtCombo\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtError != null : "fx:id=\"txtError\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtInput != null : "fx:id=\"txtInput\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtOutput != null : "fx:id=\"txtOutput\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtSeconds != null : "fx:id=\"txtSeconds\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
+    
+   
+    public void setModel(Dictionary model) {
+    	
+    	txtInput.setText("Selezionare una lingua");
+    	
+    	txtCombo.getItems().addAll("English","Italian");
+
+    	this.model=model;
+    	
+    	
+    }
+
+}
 
---
+++ src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -0,0 +1,65 @@
+package it.polito.tdp.spellchecker.model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Dictionary {
+	
+	private List<String> dizionario;
+	private String language;
+	
+	public boolean loadDictionary(String language) {
+		
+		if(dizionario!=null && this.language.equals(language)) {
+			return true;
+		}
+		
+		dizionario = new ArrayList<String>();
+		this.language=language;
+
+
+		try {
+			
+			FileReader fr = new FileReader("src/main/resources/"+language+".txt");
+			BufferedReader br = new BufferedReader(fr); 
+			String word;
+			
+			while ((word = br.readLine()) != null) {
+				dizionario.add(word.toLowerCase());
+			     }
+			
+				Collections.sort(dizionario);
+			    br.close();
+				return true;
+					
+			} catch (IOException e){
+			System.out.println("Errore nella lettura del file");
+			return false;
+			}
+
+}
+	
+	public List<RichWord> spellCheckText(List<String> inputTextList){
+		
+		List<RichWord> parole = new ArrayList<RichWord>();
+		
+
+		for (String s: inputTextList) {
+			RichWord richword = new RichWord(s);
+			if (dizionario.contains(s)) {
+				richword.setCorrect(true);
+			}
+			else
+			richword.setCorrect(false);
+			
+			parole.add(richword);
+		}
+		return parole;
+	}
+
+
+}
